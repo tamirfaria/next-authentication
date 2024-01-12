03b37d6fa5a81a504cac4b0c60a108ff15f4ac6e
Lucas Tamir Faria <lucastamir@gmail.com>
2024-01-12 10:07:16 -0300

    feat: creating a token manager service

--- frontend/pages/auth-page-ssr.jsx
+++ frontend/pages/auth-page-ssr.jsx
@@ -1,9 +1,30 @@
+import nookies from "nookies";
+import { tokenService } from "../src/services/token/token-service";
+
 const AuthPageSSR = () => {
+  const accessToken = tokenService.get();
   return (
     <div>
       <h1>Auth Page Server Side Render</h1>
+      <pre>{JSON.stringify(accessToken, null, 2)}</pre>
     </div>
   );
 };
 
 export default AuthPageSSR;
+
+export async function getServerSideProps(ctx) {
+  const cookies = nookies.get(ctx);
+  console.log({ cookies });
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  };
+}
+
+// Se tentarmos chamar a função tokenService.get() nessa página e dermos um console.log() nossa aplicação irá quebrar. Isso por conta da página rodar no servidor, e o tokenService usa funções nativas do navegador.AuthPageSSR
+
+// Para tratar esse problema usaremos cookies.
+
+// Sempre que precisarmos salvar um token em aplicações SSR, é necessário usar cookies
--- frontend/src/services/auth/auth-service.js
+++ frontend/src/services/auth/auth-service.js
@@ -1,4 +1,6 @@
 import { httpClient } from "../../infra/client/http-client";
+import { tokenService } from "../token/token-service";
+
 export const authService = {
   async login({ username, password }) {
     return httpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
@@ -6,7 +8,9 @@ export const authService = {
       body: { username, password },
     }).then(async (response) => {
       if (!response.ok) throw new Error("Username or password invalid");
-      console.log(response.body);
+      const body = response.body;
+      const accessToken = body.data.access_token;
+      tokenService.save(accessToken);
     });
   },
 };
